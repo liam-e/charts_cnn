897ae2e28b74751a88b23d05597f77f60e363b7c
Liam Ensbey <ensbeyliam@myvuw.ac.nz>
2020-12-08 21:20:01 +1300

    Saving of csvs

--- data_prep.py
+++ data_prep.py
@@ -28,16 +28,20 @@ def percent_change(a, b):
     return (b - a) / a * 100
 
 
-def main():
+def date_parse(d):
+    return dt.datetime.strptime(d, "%Y-%m-%d")
 
-    logging.basicConfig(filename=f"data_prep_{dt.datetime.now().strftime('%Y%m%d%H%M%S%f')}.log", format='%(asctime)s %(message)s', level=logging.INFO)
+
+def generate_charts():
+    logging.basicConfig(filename=f"data_prep_{dt.datetime.now().strftime('%Y%m%d%H%M%S%f')}.log",
+                        format='%(asctime)s %(message)s', level=logging.INFO)
+
+    mc = mpf.make_marketcolors(up='#00ff00', down='#ff0000', inherit=True)
+    s = mpf.make_mpf_style(base_mpf_style='nightclouds', marketcolors=mc)
+    kwargs = dict(type='candle', style=s, mav=(10, 30), volume=True, axisoff=True, figscale=0.5)
 
     w, h = 259, 194
     x, y = 87, 41
-
-    mc = mpf.make_marketcolors(up='#00ff00', down='#ff0000', inherit=True)
-
-    s = mpf.make_mpf_style(base_mpf_style='nightclouds', marketcolors=mc)
 
     with closing(request.urlopen('ftp://ftp.nasdaqtrader.com/symboldirectory/nasdaqlisted.txt')) as r:
         with open('data/nasdaqlisted.txt', 'wb') as f:
@@ -54,7 +58,7 @@ def main():
 
     print("Number of symbols total:", len(symbols))
 
-    chunk_size = 50
+    chunk_size = 5
 
     chunk_count = len(list(range(0, len(symbols), chunk_size)))
 
@@ -70,49 +74,57 @@ def main():
         training_data = []
 
         npy_file_name = f"{directory}/{chunk_i}.npy"
+        if os.path.exists(npy_file_name):
+            continue
 
         logging.info(f'Saving chunk {chunk_i} of {chunk_count}, number of symbols in chunk: {len(symbols_in_chunk)} ...')
 
         for symbol in tqdm(symbols_in_chunk):
+            df_file_name = f"data/csv/{symbol}.csv"
             try:
-                disable_print()
-                df = pdr.get_data_yahoo(symbol, dt.datetime(2000, 1, 1), dt.datetime(2020, 10, 1))
-                # df.to_csv(f"data/csv/{symbol}.csv")
-                enable_print()
+                if os.path.exists(df_file_name):
+                    df = pd.read_csv(df_file_name, index_col=0, parse_dates=True, date_parser=date_parse)
+                else:
+                    disable_print()
+                    df = pdr.get_data_yahoo(symbol, dt.datetime(2000, 1, 1), dt.datetime(2020, 10, 1))
+                    df.reset_index(level=0).to_csv(df_file_name, index=False, date_format="%Y-%m-%d")
+                    enable_print()
                 if df is None or len(df) == 0:
                     raise FileNotFoundError
             except Exception as e:
+                print(e)
                 logging.info(f"{symbol} failed download")
                 continue
-            for year in range(1998, 2021):
+
+            if df is None or len(df) == 0:
+                continue
+            for year in range(2000, 2021):
                 for month in range(1, 13, 3):
                     try:
                         df_3_month = df[(df.index.year == year) & (df.index.month >= month) & (df.index.month <= month+2)]
-                        if len(df_3_month) < 59:
+                        if len(df_3_month) < 59 or df_3_month["Volume"].min() == df_3_month["Volume"].max():
                             continue
 
                         df_X = df_3_month[:-6]
-                        df_y = df_3_month[-6:]
 
                         file_name = f"{symbol}_{year}_{month}.png"
 
-                        mpf.plot(df_X, type='candle', style=s, mav=(10, 30), volume=True, axisoff=True, figscale=0.5,
-                                 savefig=file_name)
+                        mpf.plot(df_X, **kwargs, savefig=file_name)
 
                         img = cv2.imread(file_name)
                         os.remove(file_name)
 
                         cropped_img = img[y:y+h, x:x+w]
 
-                        perc_change = percent_change(df_y.iloc[0]["Adj Close"], df_y.iloc[-1]["Adj Close"])
+                        perc_change = percent_change(df_3_month.iloc[-6]["Adj Close"], df_3_month.iloc[-1]["Adj Close"])
 
                         training_data.append([cropped_img, perc_change])
 
                         # cv2.imshow("", cropped_img)
                         # cv2.waitKey(0)
                     except Exception as e:
+                        print(e)
                         pass
-                        # logging.info(f"{symbol} failed for year {month} month {month}")
 
         np.save(npy_file_name, training_data, allow_pickle=True)
         logging.info(f'Saved chunk {chunk_i} of {chunk_count}, length {len(training_data)}')
@@ -121,4 +133,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    generate_charts()
